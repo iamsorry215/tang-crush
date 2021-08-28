d3f53bd7f178aa99fce598d530b9244c05b188c8
iamsorry215 <89695322+iamsorry215@users.noreply.github.com>
2021-08-28 21:29:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const CONFIG = {
-    introTitle: 'Babe à!',
+const CONFIG = 
+    introTitle: 'Nhy à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -16,4 +16,3 @@ const CONFIG = {
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
