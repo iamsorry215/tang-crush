d99475f5dbbcb26e6f1e72aa6cf9f8b7d4205078
iamsorry215 <89695322+iamsorry215@users.noreply.github.com>
2021-08-29 09:04:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
     introTitle: 'Ngân iu!',
-    introDesc: 'Em là chiếc lá nhỏ
-    Anh là bầu trời xanh
-    Cái gì anh cũng có
-    Chỉ thiếu mỗi mình em.',
+    introDesc: 'Em là chiếc lá nhỏ Anh là bầu trời xanh Cái gì anh cũng có Chỉ thiếu mỗi mình em.',
     btnIntro: '=))',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
