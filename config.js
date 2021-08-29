cbccd39a7e8a366fe2403eeca524e924f3cabfa8
iamsorry215 <89695322+iamsorry215@users.noreply.github.com>
2021-08-29 08:58:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Nhy à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng nhy là tiên nữ trên trời 😙',
-    desc: 'Phải chăng có tiên nữ là ú như nhy ',
-    btnYes: 'nhy bị ú <33',
-    btnNo: 'nhy ko bị ú:3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao có mỗi nhy bị ú <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'ú thì ú mà không ú thì mập <33333333',
-    mess: 'Mị biết mà 🥰. nhy bị mập nhiều nhiều 😘😘',
+    introTitle: 'Ngân iu!',
+    introDesc: `TEm là chiếc lá nhỏ
+Anh là bầu trời xanh
+Cái gì anh cũng có
+Chỉ thiếu mỗi mình em.,
+    btnIntro: '=))',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Ngân cục súk =))',
+    btnNo: 'Ngân cute, xik gái',
+    question:'Trên thế giới hơn 7 tỉ người mà sao a lại yêu e <3',
+    btnReply: 'Gửi cho e <3',
+    reply: 'giận thì giận nhưng yêu thì mãi yêu <3',
+    mess: 'T biết mà 🥰. M yêu t nhiều 😘😘',
     messDesc: 'đúng không nhở?.',
     btnAccept: 'quá đúng lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
